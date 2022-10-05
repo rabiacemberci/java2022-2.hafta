a04ff3cc22fb8636f4ed9df654bdf4874daf6d4b
rabiacemberci <rabiacemberci3@gmail.com>
2022-10-05 14:06:23 +0300

    java2022-2.hafta

---
+++ javademos2/methodOverloading/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	DortIslme dortIslme = new DortIslme();
+	System.out.println(dortIslme.topla(2,3));
+	System.out.println(dortIslme.topla(4,5,6));
+    }
+}
